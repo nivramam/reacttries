3f88dca017b859bc69a05413454b7289fc8f566f
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2022-09-04 21:36:31 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PDFRenderComponent from './components/PDFRenderComponent';
+import PDFbyPSPDFkit from './components/PDFbyPSPDFkit';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <Router>
+          <Routes>
+            <Route path='/' element={<PDFRenderComponent/>} />
+            <Route path='/' element={<PDFbyPSPDFkit/>} />
+          </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PDFRenderComponent.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
+export default function PDFRenderComponent() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file="./3rdgrouptest.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+}
